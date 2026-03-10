5754648c862b5baf7506942b399b34859083665b
onyourjie <filzahmufidah@student.ub.ac.id>
2026-03-10 14:55:30 +0700

    feat : layout and content for homepage

--- src/app.d.ts
+++ src/app.d.ts
@@ -10,4 +10,19 @@ declare global {
 	}
 }
 
+declare module '*.png' {
+	const src: string;
+	export default src;
+}
+
+declare module '*.jpg' {
+	const src: string;
+	export default src;
+}
+
+declare module '*.jpeg' {
+	const src: string;
+	export default src;
+}
+
 export {};
